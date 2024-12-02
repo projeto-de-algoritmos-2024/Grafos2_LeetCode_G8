c1061b08bf7dc2660dee5f9300466c8e0e097675
DeM4rcio <henriquemarcio170@gmail.com>
2024-12-01 23:00:31 -0300

    dijkstra with heap

--- modify -graph-edge-weights/solution.c
+++ modify -graph-edge-weights/solution.c
@@ -1,60 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <stdbool.h>
 
-#define INF 2000000000 // ----"infinito"----
+#define INF LLONG_MAX
 
 
-long long dijkstra(int** edges, int edgesSize, int n, int src, int dest) {
-    long long** graph = (long long**)malloc(n * sizeof(long long*));
-    long long* distance = (long long*)malloc(n * sizeof(long long));
-    int* visited = (int*)malloc(n * sizeof(int));
+typedef struct Node {
+    int vertex;
+    long long weight;
+    struct Node* next;
+} Node;
 
+
+Node* createNode(int vertex, long long weight) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->vertex = vertex;
+    newNode->weight = weight;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void freeGraph(Node** graph, int n) {
     for (int i = 0; i < n; ++i) {
-        graph[i] = (long long*)malloc(n * sizeof(long long));
-        for (int j = 0; j < n; ++j) {
-            graph[i][j] = INF;
+        Node* current = graph[i];
+        while (current) {
+            Node* temp = current;
+            current = current->next;
+            free(temp);
         }
-        distance[i] = INF;
-        visited[i] = 0;
     }
+    free(graph);
+}
+
+
+long long dijkstra(Node** graph, int n, int src, int dest) {
+    long long* distance = (long long*)malloc(n * sizeof(long long));
+    bool* visited = (bool*)malloc(n * sizeof(bool));
+    
+    for (int i = 0; i < n; ++i) {
+        distance[i] = INF;
+        visited[i] = false;
+    }
+
     distance[src] = 0;
 
-    // ---insere grafo----
-    for (int i = 0; i < edgesSize; ++i) {
-        int from = edges[i][0], to = edges[i][1], weight = edges[i][2];
-        if (weight == -1) continue; 
-        graph[from][to] = weight;
-        graph[to][from] = weight;
-    }
-
-    // ----Dijkstra força bruta----
     for (int i = 0; i < n; ++i) {
-        int closestUnvisitedNode = -1;
+        int u = -1;
         for (int j = 0; j < n; ++j) {
-            if (!visited[j] && (closestUnvisitedNode == -1 || distance[j] < distance[closestUnvisitedNode])) {
-                closestUnvisitedNode = j;
+            if (!visited[j] && (u == -1 || distance[j] < distance[u])) {
+                u = j;
             }
         }
 
-        visited[closestUnvisitedNode] = 1;
+        if (distance[u] == INF) break;
 
-        for (int j = 0; j < n; ++j) {
-            if (graph[closestUnvisitedNode][j] != INF) {
-                distance[j] = distance[j] < distance[closestUnvisitedNode] + graph[closestUnvisitedNode][j]
-                                  ? distance[j]
-                                  : distance[closestUnvisitedNode] + graph[closestUnvisitedNode][j];
+        visited[u] = true;
+        Node* current = graph[u];
+        while (current) {
+            int v = current->vertex;
+            long long weight = current->weight;
+            if (distance[u] != INF && distance[u] + weight < distance[v]) {
+                distance[v] = distance[u] + weight;
             }
+            current = current->next;
         }
     }
 
     long long result = distance[dest];
-
-    
-    for (int i = 0; i < n; ++i) {
-        free(graph[i]);
-    }
-    free(graph);
     free(distance);
     free(visited);
 
@@ -63,50 +76,104 @@ long long dijkstra(int** edges, int edgesSize, int n, int src, int dest) {
 
 
 int** modifiedGraphEdges(int n, int** edges, int edgesSize, int* edgesColSize, int source, int destination, int target, int* returnSize, int** returnColumnSizes) {
-    long long shortestDistance = dijkstra(edges, edgesSize, n, source, destination);
+    Node** graph = (Node**)malloc(n * sizeof(Node*));
+    for (int i = 0; i < n; ++i) {
+        graph[i] = NULL;
+    }
 
-    if (shortestDistance < target) { 
+ 
+    for (int i = 0; i < edgesSize; ++i) {
+        int from = edges[i][0];
+        int to = edges[i][1];
+        int weight = edges[i][2];
+        Node* newNode1 = createNode(to, weight);
+        newNode1->next = graph[from];
+        graph[from] = newNode1;
+
+        Node* newNode2 = createNode(from, weight);
+        newNode2->next = graph[to];
+        graph[to] = newNode2;
+    }
+
+   
+    long long shortestDistance = dijkstra(graph, n, source, destination);
+    if (shortestDistance == target) {
+       
+        *returnSize = edgesSize;
+        *returnColumnSizes = (int*)malloc(edgesSize * sizeof(int));
+        return edges;
+    }
+
+  
+    for (int i = 0; i < edgesSize; ++i) {
+        if (edges[i][2] == -1) {
+           
+            int low = 1, high = 1000000000;  
+            int bestWeight = -1;
+
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                edges[i][2] = mid;
+
+               
+                Node** newGraph = (Node**)malloc(n * sizeof(Node*));
+                for (int j = 0; j < n; ++j) newGraph[j] = NULL;
+
+                for (int j = 0; j < edgesSize; ++j) {
+                    int from = edges[j][0], to = edges[j][1], weight = edges[j][2];
+                    Node* newNode1 = createNode(to, weight);
+                    newNode1->next = newGraph[from];
+                    newGraph[from] = newNode1;
+
+                    Node* newNode2 = createNode(from, weight);
+                    newNode2->next = newGraph[to];
+                    newGraph[to] = newNode2;
+                }
+
+              
+                shortestDistance = dijkstra(newGraph, n, source, destination);
+                if (shortestDistance < target) {
+                    low = mid + 1;
+                } else {
+                    bestWeight = mid;
+                    high = mid - 1;
+                }
+
+                freeGraph(newGraph, n);
+            }
+
+            if (bestWeight != -1) {
+                edges[i][2] = bestWeight;
+            }
+        }
+    }
+
+   
+    Node** finalGraph = (Node**)malloc(n * sizeof(Node*));
+    for (int j = 0; j < n; ++j) finalGraph[j] = NULL;
+
+    for (int j = 0; j < edgesSize; ++j) {
+        int from = edges[j][0], to = edges[j][1], weight = edges[j][2];
+        Node* newNode1 = createNode(to, weight);
+        newNode1->next = finalGraph[from];
+        finalGraph[from] = newNode1;
+
+        Node* newNode2 = createNode(from, weight);
+        newNode2->next = finalGraph[to];
+        finalGraph[to] = newNode2;
+    }
+
+    shortestDistance = dijkstra(finalGraph, n, source, destination);
+    freeGraph(finalGraph, n);
+
+    if (shortestDistance == target) {
+        *returnSize = edgesSize;
+        *returnColumnSizes = (int*)malloc(edgesSize * sizeof(int));
+        return edges;
+    } else {
+      
         *returnSize = 0;
         *returnColumnSizes = NULL;
         return NULL;
     }
-
-    int isEqualToTarget = (shortestDistance == target);
-
-    for (int i = 0; i < edgesSize; ++i) {
-        if (edges[i][2] > 0) continue; 
-
-        if (isEqualToTarget) { 
-            edges[i][2] = INF;
-            continue;
-        }
-
-        
-        edges[i][2] = 1;
-        shortestDistance = dijkstra(edges, edgesSize, n, source, destination);
-
-        if (shortestDistance <= target) { 
-            isEqualToTarget = 1;
-            edges[i][2] += (target - shortestDistance);
-        }
-    }
-
-    if (!isEqualToTarget) {
-        *returnSize = 0;
-        *returnColumnSizes = NULL;
-        return NULL;
-    }
-
-    int** result = (int**)malloc(edgesSize * sizeof(int*));
-    *returnColumnSizes = (int*)malloc(edgesSize * sizeof(int));
-    for (int i = 0; i < edgesSize; ++i) {
-        result[i] = (int*)malloc(3 * sizeof(int));
-        (*returnColumnSizes)[i] = 3;
-        result[i][0] = edges[i][0];
-        result[i][1] = edges[i][1];
-        result[i][2] = edges[i][2];
-    }
-
-    *returnSize = edgesSize;
-    return result;
 }
